9b5224ab5e45e0978668eba9c9110c7dfcfc4e62
ananthu999 <147549590+ananthu999@users.noreply.github.com>
2023-10-11 03:15:34 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -38,8 +38,7 @@ class LoginView(APIView):
         response=Response()
         response.set_cookie(key='jwt',value=token,httponly=True)
         response.data={
-            'jwt':token,
-            
+            'jwt':token
         }
         # print(datetime.datetime.utcnow)
         return response
